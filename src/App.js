364604b1e2865325a92571ce9158193db9b610f4
ryankells <rtkells@mail.lipscomb.edu>
2023-12-01 13:15:07 -0600

    Updated clock and added functional search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { useEffect, useState } from "react";
 import { Clock } from "./components/clock.js"
+import { SketchPicker } from "react-color";
+import { SearchBar } from "./components/searchBar.js"
 import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [currentColor, setCurrentColor] = useState("")
+
   return (
     <div className="App">
-      <Clock />
+      <SearchBar />
+      <SketchPicker 
+      color={currentColor}
+      onChange={(event) => {setCurrentColor(event.hex)}}
+      />
+      <Clock color={currentColor}/>
     </div>
   );
 }
---
+++ src/components/searchBar.js
@@ -0,0 +1,19 @@
+import "./searchBar.css";
+import {useState,useEffect} from "react";
+
+export function SearchBar() {
+
+    const search = (e) => {
+        if (e.keyCode === 13) {
+            window.location.href = "https://www.google.com/search?q=" + encodeURIComponent(e.target.value);
+        }
+    }
+
+    return (
+        <div className="SearchBar">
+            <input type="text" placeholder="Search with Google" onKeyDown={(e) => search(e)}></input>
+        </div>
+    )
+}
+
+export default SearchBar;
