6f9ec76565552ab2aaa080c25c2189c5e49589bb
eband <147673950+ebanderson3@users.noreply.github.com>
2023-12-01 15:55:36 -0600

    Added variety of background images

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 import { Clock } from "./components/clock.js"
 import { SketchPicker } from "react-color";
 import { SearchBar } from "./components/searchBar.js"
+import { background_list } from "./components/backgroundList.js"
 import "./App.css";
 
 function App() {
   const [currentColor, setCurrentColor] = useState("")
 
-  let background_url = "https://images.unsplash.com/photo-1617558541906-6512230bb94f?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  let background_url = background_list[Math.floor(Math.random() * background_list.length)];
 
   return (
     <div className="App" style={{backgroundImage: `url(${background_url})`}}>
