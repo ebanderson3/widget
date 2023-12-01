ac7b506ad6aa7bc22ff7a429574c12fe82c2d864
eband <147673950+ebanderson3@users.noreply.github.com>
2023-12-01 14:28:33 -0600

    Implemented simple background image

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import "./App.css";
 function App() {
   const [currentColor, setCurrentColor] = useState("")
 
+  let background_url = "https://images.unsplash.com/photo-1617558541906-6512230bb94f?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
   return (
-    <div className="App">
+    <div className="App" style={{background: `url(${background_url})`}}>
       <SearchBar />
       <SketchPicker 
       color={currentColor}
